031a23844f61307eb6b5f633ac2795376c52a923
Francisco Rubial <franjorub@gmail.com>
2021-06-06 02:19:32 +0000

    Installed pg, aws-S3 upload provider and updated the config files

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+	defaultConnection: 'default',
+	connections: {
+		default: {
+			connector: 'bookshelf',
+			settings: {
+				client: 'postgres',
+				host: env('DATABASE_HOST', '127.0.0.1'),
+				port: env.int('DATABASE_PORT', 5432),
+				database: env('DATABASE_NAME', 'blogadmin-db'),
+				username: env('DATABASE_USERNAME', 'postgres'),
+				password: env('DATABASE_PASSWORD', 'postgres'),
+			},
+			options: {
+				ssl: false,
+			},
+		},
+	},
 });
