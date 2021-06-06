d9b1117a7df614a33959b4ad4f7f639c6dff6fdf
Francisco Rubial <franjorub@gmail.com>
2021-06-06 16:34:14 +0000

    Fix config

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
 			connector: 'bookshelf',
 			settings: {
 				client: 'postgres',
-				host: env('DATABASE_HOST', '127.0.0.1'),
+				host: env('DATABASE_HOST', 'blogadmin.cocsqhxalrmd.us-east-1.rds.amazonaws.com'),
 				port: env.int('DATABASE_PORT', 5432),
-				database: env('DATABASE_NAME', 'blogadmin-db'),
+				database: env('DATABASE_NAME', 'blogadmin'),
 				username: env('DATABASE_USERNAME', 'postgres'),
 				password: env('DATABASE_PASSWORD', 'postgres'),
 			},
