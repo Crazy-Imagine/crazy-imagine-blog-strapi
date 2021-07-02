a14e1c77d89ce0d19f044825dfb17d0591a3b130
Francisco Rubial <franjorub@gmail.com>
2021-07-02 14:29:19 -0400

    feat: adding lower case for slugs

--- api/members/models/members.js
+++ api/members/models/members.js
@@ -9,12 +9,12 @@ module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.name) {
-        data.slug = slugify(data.name);
+        data.slug = slugify(data.name, { lower: true });
       }
     },
     beforeUpdate: async (params, data) => {
       if (data.name) {
-        data.slug = slugify(data.name);
+        data.slug = slugify(data.name, { lower: true });
       }
     },
   },
